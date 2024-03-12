e92aa99151d179cb3ece694ca7a24ea7deceb18c
togay-aytemiz <togayaytemiz@gmail.com>
2024-03-12 17:33:34 +0300

    add bmi calculation

--- app/page.js
+++ app/page.js
@@ -1,113 +1,106 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useMemo } from "react";
 
 export default function Home() {
+  const [weight, setWeight] = useState(72);
+  const [height, setHeight] = useState(180);
+
+  const calculateBMI = () => {
+    let heightInKg = height / 100;
+    let bmi = weight / (heightInKg * heightInKg);
+    return bmi;
+  };
+
+  const normalizeBMI = (bmi) => {
+    const minBMI = 15; // Minimum possible BMI
+    const maxBMI = 40; // Maximum possible BMI
+    return (bmi - minBMI) / (maxBMI - minBMI);
+  };
+
+  const getProgressColor = (bmi) => {
+    if (bmi < 18.5) {
+      return "text-blue-500"; // Underweight
+    } else if (bmi >= 18.5 && bmi < 25) {
+      return "text-green-500"; // Normal weight
+    } else if (bmi >= 25 && bmi < 30) {
+      return "text-yellow-500"; // Overweight
+    } else {
+      return "text-red-500"; // Obese
+    }
+  };
+
+  const memoizedBMI = useMemo(() => calculateBMI(), [weight, height]);
+  const memoizedNormalizedBMI = useMemo(
+    () => normalizeBMI(memoizedBMI),
+    [memoizedBMI]
+  );
+  const memoizedProgressColor = useMemo(
+    () => getProgressColor(memoizedBMI),
+    [memoizedBMI]
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <main className="my-5 sm:mx-auto mx-5 max-w-md p-5 pb-10 bg-white rounded-lg shadow-sm">
+        <div className="mb-10">
+          <h1 className="text-xl font-light">
+            <span className="font-bold">BMI Tracker</span>
+            <br />
+            Monitor Your Health with Precision
+          </h1>
         </div>
-      </div>
+        <div className="flex flex-col items-start justify-center gap-5 w-full">
+          {/* Kilo */}
+          <div className="flex flex-col items-start justify-center gap-3  w-full mb-3">
+            <label htmlFor="weight" className="text-xl font-medium">
+              Kilo: {weight} kg
+            </label>
+            <input
+              type="range"
+              min={40}
+              max="200"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+              className="range range-info"
+              id="weight"
+            />
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          {/* Boy */}
+          <div className="flex flex-col items-start justify-center gap-3  w-full">
+            <label htmlFor="weight" className="text-xl font-medium">
+              Boy: {height} cm
+            </label>
+            <input
+              type="range"
+              min={80}
+              max="220"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              className="range range-info"
+              id="weight"
+            />
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="divider my-10"></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {/* BMI */}
+        <div className="flex items-center flex-col gap-10">
+          <h1 className="text-2xl font-bold">Your calculated BMI</h1>
+          <div
+            className={`radial-progress ${memoizedProgressColor}`}
+            style={{
+              "--value": memoizedNormalizedBMI * 100,
+              "--size": "12rem",
+            }}
+            role="progressbar"
+          >
+            <span className="text-2xl font-bold">{memoizedBMI.toFixed(1)}</span>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
